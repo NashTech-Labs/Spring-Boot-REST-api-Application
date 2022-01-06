fcd9f106b5b6bd6b8d6dd4de913f7d07b3c1dcae
Shashikant Tanti <88486204+Shashikant-15@users.noreply.github.com>
2022-01-06 22:28:36 +0530

    Update ApplicationController.java

--- src/main/java/com/knoldus/springBootApi/currentTime/ControllerClass/ApplicationController.java
+++ src/main/java/com/knoldus/springBootApi/currentTime/ControllerClass/ApplicationController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+* Represents a controller class of 1-api  Application.
+*/
 @RestController
 public class ApplicationController {
-
-    // inserting ApplicationServices object
+    /**
+    * inserting ApplicationServices object
+    */
     @Autowired
     ApplicationServices appService;
 
